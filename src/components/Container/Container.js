8dc19a2dac6891eefae7013c10619bd3fa3f4c4a
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-02-01 20:36:20 -0700

    feat: create Container for child App components

---
+++ src/components/Container/Container.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/macro";
+import { QUERIES } from "../../constants";
+
+const Container = styled.div`
+  width: 1100px;
+  max-width: 100%;
+  padding: 0 24px;
+  margin: 0 auto;
+  @media ${QUERIES.tabletAndUp} {
+    padding: 0 40px;
+  }
+`;
+
+export default Container;
