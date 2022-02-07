ac33f70e87052f2e5fd49538df2ed7ef51feb730
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-02-07 15:08:00 -0700

    style: add background image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,27 @@
 import Header from "../Header";
+import styled from "styled-components/macro";
 import Container from "../Container";
+import BgPatternHeader2 from "../../assets/desktop/bg-pattern-header.svg";
 
 function App() {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <BgHeader />
+      <Container>
+        <Header />
+      </Container>
+    </>
   );
 }
 
 export default App;
+
+const BgHeader = styled.div`
+  background-image: url(${BgPatternHeader2});
+  height: 160px;
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+`;
