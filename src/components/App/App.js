dc4e9a8e46a38f5fc42a792c580a192e8e13d9bd
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-02-10 16:22:23 -0700

    style: add bg-pattern-header on different screen sizes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,10 @@
 import Header from "../Header";
 import styled from "styled-components/macro";
 import Container from "../Container";
-import BgPatternHeader2 from "../../assets/desktop/bg-pattern-header.svg";
+import { QUERIES } from "../../constants";
+import BgPatternHeaderMobile from "../../assets/mobile/bg-pattern-header.svg";
+import BgPatternHeaderTablet from "../../assets/tablet/bg-pattern-header.svg";
+import BgPatternHeaderDesktop from "../../assets/desktop/bg-pattern-header.svg";
 
 function App() {
   return (
@@ -17,11 +20,20 @@ function App() {
 export default App;
 
 const BgHeader = styled.div`
-  background-image: url(${BgPatternHeader2});
-  height: 160px;
+  background-image: url(${BgPatternHeaderMobile});
+  height: 136px;
   background-size: cover;
   position: absolute;
   top: 0;
   right: 0;
   left: 0;
+
+  @media ${QUERIES.tabletAndUp} {
+    background-image: url(${BgPatternHeaderTablet});
+    height: 160px;
+  }
+
+  @media ${QUERIES.desktopAndUp} {
+    background-image: url(${BgPatternHeaderDesktop});
+  }
 `;
