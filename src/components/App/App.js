90c30f7afe4aaa32ead339e64f6508e4c68658d2
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-02-10 16:30:28 -0700

    refactor: move BgHeader into it's own folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,6 @@
 import Header from "../Header";
-import styled from "styled-components/macro";
 import Container from "../Container";
-import { QUERIES } from "../../constants";
-import BgPatternHeaderMobile from "../../assets/mobile/bg-pattern-header.svg";
-import BgPatternHeaderTablet from "../../assets/tablet/bg-pattern-header.svg";
-import BgPatternHeaderDesktop from "../../assets/desktop/bg-pattern-header.svg";
+import BgHeader from "../BgHeader";
 
 function App() {
   return (
@@ -18,22 +14,3 @@ function App() {
 }
 
 export default App;
-
-const BgHeader = styled.div`
-  background-image: url(${BgPatternHeaderMobile});
-  height: 136px;
-  background-size: cover;
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-
-  @media ${QUERIES.tabletAndUp} {
-    background-image: url(${BgPatternHeaderTablet});
-    height: 160px;
-  }
-
-  @media ${QUERIES.desktopAndUp} {
-    background-image: url(${BgPatternHeaderDesktop});
-  }
-`;
