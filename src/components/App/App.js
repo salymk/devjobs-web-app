1bba56240a3de244692b7101261c7d8670225ff9
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-03-08 07:38:56 -0700

    feat: add desktop search bar to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,35 @@
+import React from "react";
 import whatInput from "what-input";
 import Header from "../Header";
 import Container from "../Container";
 import BgHeader from "../BgHeader";
 import SearchBar from "../SearchBar";
 import Jobs from "../Jobs";
+import DesktopSearchBar from "../DesktopSearchBar/DesktopSearchBar";
 
 function App() {
+  const [contract, setContract] = React.useState(false);
+  const [title, setTitle] = React.useState("");
+  const [location, setLocation] = React.useState("");
+
+  const handleSubmit = (values) => {
+    console.log(values.title, values.location, values.contract);
+  };
+
   return (
     <>
       <BgHeader />
       <Container>
         <Header />
-        <SearchBar />
-        <Jobs />
+        <SearchBar>
+          <DesktopSearchBar
+            title={title}
+            location={location}
+            contract={contract}
+            handleSubmit={handleSubmit}
+          />
+        </SearchBar>
+        <Jobs title={title} />
       </Container>
     </>
   );
