e80b52dbc345b212d4fd88c290553770cc6cb82f
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-03-24 15:20:41 -0600

    feat: configure react-router-dom and react-query

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 import GlobalStyles from "./components/GlobalStyles/GlobalStyles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./components/App";
+import Job from "./components/Job";
+
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <GlobalStyles />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path=":jobId" element={<Job />} />
+      </Routes>
+      <GlobalStyles />
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
   document.getElementById("root")
 );
