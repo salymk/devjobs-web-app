a93186864474c557c39c443876fe5324698e0360
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-03-24 15:23:08 -0600

    refactor: use useJobs hook to get data

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,47 +1,63 @@
 import React from "react";
 import styled from "styled-components/macro";
-import { QUERIES } from "../../constants";
+import { COLORS, QUERIES, WEIGHTS } from "../../constants";
 import JobCard from "../JobCard/JobCard";
 import Button from "../Button/Button";
-import data from "../../data.json";
+// import data from "./data.json";
+import useJobs from "../../hooks/useJobs";
 
 const Jobs = ({ title, location, contract }) => {
+  // const [data, setData] = React.useState([]);
   const [loadMore, setLoadMore] = React.useState(6);
+  const { status, data, error, isFetching } = useJobs();
 
   const handleClick = () => setLoadMore(loadMore + 6);
   return (
     <>
       <Wrapper>
-        {data
-          .filter((job) => (contract ? job.contract === "Full Time" : true))
-          .filter((job) =>
-            location.trim() !== ""
-              ? job.location
-                  .toLowerCase()
-                  .includes(location.trim().toLowerCase())
-              : true
-          )
-          .filter((job) =>
-            title.trim() !== ""
-              ? job.company
-                  .toLowerCase()
-                  .includes(title.trim().toLowerCase()) ||
-                job.position.toLowerCase().includes(title.trim().toLowerCase())
-              : true
-          )
-          .slice(0, loadMore)
-          .map((job) => (
-            <JobCard
-              key={job.id}
-              logo={process.env.PUBLIC_URL + job.logo}
-              logoBackground={job.logoBackground}
-              postedAt={job.postedAt}
-              contract={job.contract}
-              position={job.position}
-              company={job.company}
-              location={job.location}
-            />
-          ))}
+        {status === "loading" ? (
+          <LoadingContainer>
+            <Loading>Loading jobs...</Loading>
+          </LoadingContainer>
+        ) : status === "error" ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            {data
+              .filter((job) => (contract ? job.contract === "Full Time" : true))
+              .filter((job) =>
+                location.trim() !== ""
+                  ? job.location
+                      .toLowerCase()
+                      .includes(location.trim().toLowerCase())
+                  : true
+              )
+              .filter((job) =>
+                title.trim() !== ""
+                  ? job.company
+                      .toLowerCase()
+                      .includes(title.trim().toLowerCase()) ||
+                    job.position
+                      .toLowerCase()
+                      .includes(title.trim().toLowerCase())
+                  : true
+              )
+              .slice(0, loadMore)
+              .map((job) => (
+                <JobCard
+                  to={`${job.id}`}
+                  key={job.id}
+                  logo={process.env.PUBLIC_URL + job.logo}
+                  logoBackground={job.logoBackground}
+                  postedAt={job.postedAt}
+                  contract={job.contract}
+                  position={job.position}
+                  company={job.company}
+                  location={job.location}
+                />
+              ))}
+          </>
+        )}
       </Wrapper>
       <ButtonContainer>
         <LoadMoreButton variant="fill" size="large" onClick={handleClick}>
@@ -53,6 +69,17 @@ const Jobs = ({ title, location, contract }) => {
 };
 
 export default Jobs;
+
+const LoadingContainer = styled.div`
+  display: grid;
+  place-items: center;
+`;
+
+const Loading = styled.h2`
+  font-size: 2rem;
+  color: ${COLORS.violet[200]};
+  font-weight: ${WEIGHTS.bold};
+`;
 
 const Wrapper = styled.div`
   display: grid;
