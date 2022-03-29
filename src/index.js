e6869df067c963f2ce631b799a4199c00e73cd06
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-03-29 16:55:52 -0600

    feat: create a ScrollToTop helper that scrolls to the top when you click a job

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import GlobalStyles from "./components/GlobalStyles/GlobalStyles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./components/App";
 import Job from "./components/Job";
+import ScrollToTop from "./helpers/ScrollToTop";
 
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
@@ -13,10 +14,12 @@ const queryClient = new QueryClient();
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route path=":jobId" element={<Job />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+          <Route path=":jobId" element={<Job />} />
+        </Routes>
+      </ScrollToTop>
       <GlobalStyles />
     </BrowserRouter>
     <ReactQueryDevtools initialIsOpen={false} />
