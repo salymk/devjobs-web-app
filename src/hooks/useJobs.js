8b057280890e5d69d06fac18a607e40e6bcd6ee5
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-04-15 07:50:39 -0600

    refactor: change url path

--- src/hooks/useJobs.js
+++ src/hooks/useJobs.js
@@ -2,12 +2,13 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const getJobs = async () => {
-  const { data } = await axios.get(process.env.PUBLIC_URL + "./data.json", {
+  const { data } = await axios.get(process.env.PUBLIC_URL + "./job/data.json", {
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
     },
   });
+
   return data;
 };
 
