3f87e9c339bde9436f3d381737ae59b0a90a3509
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-04-27 06:33:10 -0600

    feat: create a light and dark themes

---
+++ src/components/Themes/Themes.js
@@ -0,0 +1,15 @@
+import { COLORS } from "../../constants";
+
+export const LightTheme = {
+  body: "#F2F2F2",
+  cardBackground: COLORS.white,
+  header: COLORS.dark[100],
+  text: COLORS.gray[300],
+};
+
+export const DarkTheme = {
+  body: COLORS.dark[200],
+  cardBackground: COLORS.dark[100],
+  header: COLORS.white,
+  text: COLORS.gray[300],
+};
