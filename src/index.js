28133c9e81d2ce523a0cfd1550bc7d2d8de0fda6
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-04-27 19:39:50 -0600

    feat: wrap ThemeProvider around App

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-
+import { ThemeProvider } from "./helpers/ThemeProvider";
 import App from "./components/App";
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>,
   document.getElementById("root")
