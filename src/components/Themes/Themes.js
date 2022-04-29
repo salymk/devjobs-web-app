9dc9893791c8b4a0dc5b7ab6ccb5c60ee74bdb0b
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-04-29 08:06:31 -0600

    feat: add theme variables

--- src/components/Themes/Themes.js
+++ src/components/Themes/Themes.js
@@ -2,14 +2,26 @@ import { COLORS } from "../../constants";
 
 export const LightTheme = {
   body: "#F2F2F2",
-  cardBackground: COLORS.white,
-  header: COLORS.dark[100],
+  background: COLORS.white,
+  inputText: COLORS.dark[200],
+  checkbox: "hsla(219, 29%, 14%, .1)",
+  heading: COLORS.dark[100],
   text: COLORS.gray[300],
+  filterIcon: "#6E8098",
+  outlineButton: "hsla(235, 69%, 61%, 0.1)",
+  outlineButtonHover: "hsla(235, 69%, 61%, 0.35)",
+  outlineButtonText: COLORS.violet[200],
 };
 
 export const DarkTheme = {
   body: COLORS.dark[200],
-  cardBackground: COLORS.dark[100],
-  header: COLORS.white,
+  background: COLORS.dark[100],
+  inputText: COLORS.white,
+  checkbox: "#313743",
+  heading: COLORS.white,
   text: COLORS.gray[300],
+  filterIcon: "#FFFF",
+  outlineButton: " #303642",
+  outlineButtonHover: "#525861",
+  outlineButtonText: COLORS.white,
 };
